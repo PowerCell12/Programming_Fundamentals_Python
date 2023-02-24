f8048f0de34aa589d8e81a0c238075f882a68f61
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-02-24 23:22:34 +0200

    Create 05. Number Between 1 and 100.py

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/05. Number Between 1 and 100.py
@@ -0,0 +1,9 @@
+while True:
+   number = float(input())
+   hallo = 9
+
+   if 1 > number or number > 100:
+    hallo = 19
+   else:
+     print(f'The number {number} is between 1 and 100')
+     break
