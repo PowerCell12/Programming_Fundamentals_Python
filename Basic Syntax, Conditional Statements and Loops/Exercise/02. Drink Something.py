810c795520a2aff5db28963cc70fff0bece80a14
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-02-25 23:37:55 +0200

    Create 02. Drink Something.py

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/02. Drink Something.py
@@ -0,0 +1,13 @@
+age = int(input())
+
+if age < 15:
+    print("drink toddy")
+    
+if 14 < age < 19:
+    print('drink coke')
+
+if 18 < age < 22:
+    print('drink beer')
+
+if 21 < age:
+    print('drink whisky')
