7a961481336b187ede80de89374ef0fbe5586b10
Ivan <87208452+PowerCell12@users.noreply.github.com>
2023-03-01 22:58:01 +0200

    Create 01. Integer Operations.py

---
+++ Data Types and Variables/Exercise/01. Integer Operations.py
@@ -0,0 +1,12 @@
+num1 = int(input())
+num2 =  int(input())
+num3 =  int(input())
+num4 =  int(input())
+
+final = num1 + num2
+final = int(final)
+final = final / num3
+final = int(final)
+final = final * num4
+
+print(final)
